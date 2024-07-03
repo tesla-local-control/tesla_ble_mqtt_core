b6bd2ad7a605ba60626e34ac4ca7aa696fcc1b48
Pierre Belanger <baylanger@users.noreply.github.com>
2024-07-03 14:57:02 -0400

    last BLE_MAC1 hiding plain sight!

--- subroutines.sh
+++ subroutines.sh
@@ -48,7 +48,7 @@ listen_to_ble() {
   log_notice "Listening to BLE for presence"
   PRESENCE_TIMEOUT=10
   set +e
-  BLTCTL_OUT=$(bluetoothctl --timeout $PRESENCE_TIMEOUT scan on | grep -v DEL | grep $BLE_MAC1 2>&1)
+  BLTCTL_OUT=$(bluetoothctl --timeout $PRESENCE_TIMEOUT scan on | grep -v DEL 2>&1)
   set -e
   log_debug "${BLTCTL_OUT}"
   for count in $(seq $n_cars); do
