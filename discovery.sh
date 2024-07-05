e6e3fb6e08d63e6c264c1e890b3ca7eb8642102d
Pierre Belanger <baylanger@users.noreply.github.com>
2024-07-05 15:37:32 -0400

    Roll back discovery.sh

--- discovery.sh
+++ discovery.sh
@@ -1,7 +1,7 @@
 #
 # discovery.sh
 #
-function setup_auto_discovery() {
+setup_auto_discovery() {
  vin=$1
  log_notice "Setting up HA auto discovery for vin $vin"
 
@@ -9,12 +9,14 @@ function setup_auto_discovery() {
  DEV_NAME=Tesla_BLE_${vin}
 
  TOPIC_ROOT=tesla_ble/${vin}
+ SW_VERSION=0.0.10f
 
  log_debug "DEV_ID=$DEV_ID"
  log_debug "DEV_NAME=$DEV_NAME"
  log_debug "TOPIC_ROOT=$TOPIC_ROOT"
 
- echo  '{
+ eval $MOSQUITTO_PUB_BASE -t homeassistant/binary_sensor/${DEV_ID}/presence/config -m \
+  '{
    "state_topic": "'${TOPIC_ROOT}'/binary_sensor/presence",
    "device": {
     "identifiers": [
@@ -26,8 +28,12 @@ function setup_auto_discovery() {
    },
    "device_class": "presence",
    "name": "Presence",
-   "unique_id": "'${DEV_ID}'_presence"
-  } {
+   "unique_id": "'${DEV_ID}'_presence",
+   "sw_version": "'${SW_VERSION}'"
+  }'
+
+ eval $MOSQUITTO_PUB_BASE -t homeassistant/button/${DEV_ID}/generate_keys/config -m \
+  '{
    "command_topic": "'${TOPIC_ROOT}'/config",
    "device": {
     "identifiers": [
@@ -42,8 +48,12 @@ function setup_auto_discovery() {
    "payload_press": "generate_keys",
    "qos": 1,
    "unique_id": "'${DEV_ID}'_generate_keys",
-   "entity_category": "config"
-  } {
+   "entity_category": "config",
+   "sw_version": "'${SW_VERSION}'"
+  }'
+
+ eval $MOSQUITTO_PUB_BASE -t homeassistant/button/${DEV_ID}/deploy_key/config -m \
+  '{
    "command_topic": "'${TOPIC_ROOT}'/config",
    "device": {
     "identifiers": [
@@ -58,8 +68,12 @@ function setup_auto_discovery() {
    "payload_press": "deploy_key",
    "qos": 1,
    "unique_id": "'${DEV_ID}'_deploy_key",
-   "entity_category": "config"
-  } {
+   "entity_category": "config",
+   "sw_version": "'${SW_VERSION}'"
+  }'
+
+ eval $MOSQUITTO_PUB_BASE -t homeassistant/button/${DEV_ID}/scan_bluetooth/config -m \
+  '{
    "command_topic": "'${TOPIC_ROOT}'/config",
    "device": {
     "identifiers": [
@@ -74,8 +88,12 @@ function setup_auto_discovery() {
    "payload_press": "scan_bluetooth",
    "qos": 1,
    "unique_id": "'${DEV_ID}'_scan_bluetooth",
-   "entity_category": "diagnostic"
-  } {
+   "entity_category": "diagnostic",
+   "sw_version": "'${SW_VERSION}'"
+  }'
+
+ eval $MOSQUITTO_PUB_BASE -t homeassistant/button/${DEV_ID}/wake/config -m \
+  '{
    "command_topic": "'${TOPIC_ROOT}'/command",
    "device": {
     "identifiers": [
@@ -88,8 +106,12 @@ function setup_auto_discovery() {
    "name": "Wake Car",
    "payload_press": "wake",
    "qos": 1,
-   "unique_id": "'${DEV_ID}'_wake"
-  } {
+   "unique_id": "'${DEV_ID}'_wake",
+   "sw_version": "'${SW_VERSION}'"
+  }'
+
+ eval $MOSQUITTO_PUB_BASE -t homeassistant/button/${DEV_ID}/flash-lights/config -m \
+  '{
    "command_topic": "'${TOPIC_ROOT}'/command",
    "device": {
     "identifiers": [
@@ -102,8 +124,12 @@ function setup_auto_discovery() {
    "name": "Flash Lights",
    "payload_press": "flash-lights",
    "qos": 1,
-   "unique_id": "'${DEV_ID}'_flash_lights"
-  } {
+   "unique_id": "'${DEV_ID}'_flash_lights",
+   "sw_version": "'${SW_VERSION}'"
+  }'
+
+ eval $MOSQUITTO_PUB_BASE -t homeassistant/button/${DEV_ID}/honk/config -m \
+  '{
    "command_topic": "'${TOPIC_ROOT}'/command",
    "device": {
     "identifiers": [
@@ -116,8 +142,12 @@ function setup_auto_discovery() {
    "name": "Honk",
    "payload_press": "honk",
    "qos": 1,
-   "unique_id": "'${DEV_ID}'_honk"
-  } {
+   "unique_id": "'${DEV_ID}'_honk",
+   "sw_version": "'${SW_VERSION}'"
+  }'
+
+ eval $MOSQUITTO_PUB_BASE -t homeassistant/button/${DEV_ID}/lock/config -m \
+  '{
    "command_topic": "'${TOPIC_ROOT}'/command",
    "device": {
     "identifiers": [
@@ -130,8 +160,12 @@ function setup_auto_discovery() {
    "name": "Lock Car",
    "payload_press": "lock",
    "qos": 1,
-   "unique_id": "'${DEV_ID}'_lock"
-  } {
+   "unique_id": "'${DEV_ID}'_lock",
+   "sw_version": "'${SW_VERSION}'"
+  }'
+
+ eval $MOSQUITTO_PUB_BASE -t homeassistant/button/${DEV_ID}/unlock/config -m \
+  '{
    "command_topic": "'${TOPIC_ROOT}'/command",
    "device": {
     "identifiers": [
@@ -144,8 +178,12 @@ function setup_auto_discovery() {
    "name": "Unlock Car",
    "payload_press": "unlock",
    "qos": 1,
-   "unique_id": "'${DEV_ID}'_unlock"
-  } {
+   "unique_id": "'${DEV_ID}'_unlock",
+   "sw_version": "'${SW_VERSION}'"
+   }'
+
+ eval $MOSQUITTO_PUB_BASE -t homeassistant/button/${DEV_ID}/auto-seat-climate/config -m \
+  '{
    "command_topic": "'${TOPIC_ROOT}'/auto-seat-and-climate",
    "device": {
     "identifiers": [
@@ -158,8 +196,12 @@ function setup_auto_discovery() {
    "name": "Auto Seat & Climate",
    "payload_press": "auto-seat-and-climate",
    "qos": 1,
-   "unique_id": "'${DEV_ID}'_auto_seat-climate"
-  } {
+   "unique_id": "'${DEV_ID}'_auto_seat-climate",
+   "sw_version": "'${SW_VERSION}'"
+   }'
+
+ eval $MOSQUITTO_PUB_BASE -t homeassistant/button/${DEV_ID}/climate-off/config -m \
+  '{
    "command_topic": "'${TOPIC_ROOT}'/command",
    "device": {
     "identifiers": [
@@ -172,8 +214,12 @@ function setup_auto_discovery() {
    "name": "Climate Off",
    "payload_press": "climate-off",
    "qos": 1,
-   "unique_id": "'${DEV_ID}'_climate-off"
-  } {
+   "unique_id": "'${DEV_ID}'_climate-off",
+   "sw_version": "'${SW_VERSION}'"
+   }'
+
+ eval $MOSQUITTO_PUB_BASE -t homeassistant/button/${DEV_ID}/climate-on/config -m \
+  '{
    "command_topic": "'${TOPIC_ROOT}'/command",
    "device": {
     "identifiers": [
@@ -186,8 +232,12 @@ function setup_auto_discovery() {
    "name": "Climate On",
    "payload_press": "climate-on",
    "qos": 1,
-   "unique_id": "'${DEV_ID}'_climate-on"
-  } {
+   "unique_id": "'${DEV_ID}'_climate-on",
+   "sw_version": "'${SW_VERSION}'"
+   }'
+
+ eval $MOSQUITTO_PUB_BASE -t homeassistant/button/${DEV_ID}/trunk-open/config -m \
+  '{
    "command_topic": "'${TOPIC_ROOT}'/command",
    "device": {
     "identifiers": [
@@ -200,8 +250,12 @@ function setup_auto_discovery() {
    "name": "Open Trunk",
    "payload_press": "trunk-open",
    "qos": 1,
-   "unique_id": "'${DEV_ID}'_trunk-open"
-  } {
+   "unique_id": "'${DEV_ID}'_trunk-open",
+   "sw_version": "'${SW_VERSION}'"
+   }'
+
+ eval $MOSQUITTO_PUB_BASE -t homeassistant/button/${DEV_ID}/trunk-close/config -m \
+  '{
    "command_topic": "'${TOPIC_ROOT}'/command",
    "device": {
     "identifiers": [
@@ -214,8 +268,12 @@ function setup_auto_discovery() {
    "name": "Close Trunk",
    "payload_press": "trunk-close",
    "qos": 1,
-   "unique_id": "'${DEV_ID}'_trunk-close"
-  } {
+   "unique_id": "'${DEV_ID}'_trunk-close",
+   "sw_version": "'${SW_VERSION}'"
+   }'
+
+ eval $MOSQUITTO_PUB_BASE -t homeassistant/button/${DEV_ID}/frunk-open/config -m \
+  '{
    "command_topic": "'${TOPIC_ROOT}'/command",
    "device": {
     "identifiers": [
@@ -228,8 +286,12 @@ function setup_auto_discovery() {
    "name": "Open Frunk",
    "payload_press": "frunk-open",
    "qos": 1,
-   "unique_id": "'${DEV_ID}'_frunk-open"
-  } {
+   "unique_id": "'${DEV_ID}'_frunk-open",
+   "sw_version": "'${SW_VERSION}'"
+   }'
+
+ eval $MOSQUITTO_PUB_BASE -t homeassistant/button/${DEV_ID}/charging-start/config -m \
+  '{
    "command_topic": "'${TOPIC_ROOT}'/command",
    "device": {
     "identifiers": [
@@ -242,8 +304,12 @@ function setup_auto_discovery() {
    "name": "Start Charging",
    "payload_press": "charging-start",
    "qos": 1,
-   "unique_id": "'${DEV_ID}'_charging-start"
-  } {
+   "unique_id": "'${DEV_ID}'_charging-start",
+   "sw_version": "'${SW_VERSION}'"
+   }'
+
+ eval $MOSQUITTO_PUB_BASE -t homeassistant/button/${DEV_ID}/charging-stop/config -m \
+  '{
    "command_topic": "'${TOPIC_ROOT}'/command",
    "device": {
     "identifiers": [
@@ -256,8 +322,12 @@ function setup_auto_discovery() {
    "name": "Stop Charging",
    "payload_press": "charging-stop",
    "qos": 1,
-   "unique_id": "'${DEV_ID}'_charging-stop"
-  } {
+   "unique_id": "'${DEV_ID}'_charging-stop",
+   "sw_version": "'${SW_VERSION}'"
+   }'
+
+ eval $MOSQUITTO_PUB_BASE -t homeassistant/button/${DEV_ID}/charge-port-open/config -m \
+  '{
    "command_topic": "'${TOPIC_ROOT}'/command",
    "device": {
     "identifiers": [
@@ -270,8 +340,12 @@ function setup_auto_discovery() {
    "name": "Open Charge Port",
    "payload_press": "charge-port-open",
    "qos": 1,
-   "unique_id": "'${DEV_ID}'_charge-port-open"
-  } {
+   "unique_id": "'${DEV_ID}'_charge-port-open",
+   "sw_version": "'${SW_VERSION}'"
+   }'
+
+ eval $MOSQUITTO_PUB_BASE -t homeassistant/button/${DEV_ID}/charge-port-close/config -m \
+  '{
    "command_topic": "'${TOPIC_ROOT}'/command",
    "device": {
     "identifiers": [
@@ -284,8 +358,12 @@ function setup_auto_discovery() {
    "name": "Close Charge Port",
    "payload_press": "charge-port-close",
    "qos": 1,
-   "unique_id": "'${DEV_ID}'_charge-port-close"
-  } {
+   "unique_id": "'${DEV_ID}'_charge-port-close",
+   "sw_version": "'${SW_VERSION}'"
+   }'
+
+ eval $MOSQUITTO_PUB_BASE -t homeassistant/button/${DEV_ID}/windows-close/config -m \
+  '{
    "command_topic": "'${TOPIC_ROOT}'/command",
    "device": {
     "identifiers": [
@@ -298,8 +376,12 @@ function setup_auto_discovery() {
    "name": "Close Windows",
    "payload_press": "windows-close",
    "qos": 1,
-   "unique_id": "'${DEV_ID}'_windows-close"
-  } {
+   "unique_id": "'${DEV_ID}'_windows-close",
+   "sw_version": "'${SW_VERSION}'"
+   }'
+
+ eval $MOSQUITTO_PUB_BASE -t homeassistant/button/${DEV_ID}/windows-vent/config -m \
+  '{
    "command_topic": "'${TOPIC_ROOT}'/command",
    "device": {
     "identifiers": [
@@ -312,8 +394,12 @@ function setup_auto_discovery() {
    "name": "Vent Windows",
    "payload_press": "windows-vent",
    "qos": 1,
-   "unique_id": "'${DEV_ID}'_windows-vent"
-  } {
+   "unique_id": "'${DEV_ID}'_windows-vent",
+   "sw_version": "'${SW_VERSION}'"
+   }'
+
+ eval $MOSQUITTO_PUB_BASE -t homeassistant/number/${DEV_ID}/charging-set-amps/config -m \
+  '{
    "command_topic": "'${TOPIC_ROOT}'/charging-amps",
    "device": {
     "identifiers": [
@@ -330,8 +416,12 @@ function setup_auto_discovery() {
    "mode": "slider",
    "unit_of_measurement": "A",
    "qos": 1,
-   "icon": "mdi:current-ac"
-  } {
+   "icon": "mdi:current-ac",
+   "sw_version": "'${SW_VERSION}'"
+   }'
+
+ eval $MOSQUITTO_PUB_BASE -t homeassistant/number/${DEV_ID}/charging-set-limit/config -m \
+  '{
    "command_topic": "'${TOPIC_ROOT}'/charging-set-limit",
    "device": {
     "identifiers": [
@@ -348,8 +438,12 @@ function setup_auto_discovery() {
    "mode": "slider",
    "unit_of_measurement": "%",
    "qos": 1,
-   "icon": "mdi:battery-90"
-  } {
+   "icon": "mdi:battery-90",
+   "sw_version": "'${SW_VERSION}'"
+   }'
+
+ eval $MOSQUITTO_PUB_BASE -t homeassistant/number/${DEV_ID}/climate-temp/config -m \
+  '{
    "command_topic": "'${TOPIC_ROOT}'/climate-set-temp",
    "device": {
     "identifiers": [
@@ -366,8 +460,12 @@ function setup_auto_discovery() {
    "mode": "slider",
    "unit_of_measurement": "°C",
    "qos": 1,
-   "icon": "mdi:temperature"
-  } {
+   "icon": "mdi:temperature",
+   "sw_version": "'${SW_VERSION}'"
+   }'
+
+ eval $MOSQUITTO_PUB_BASE -t homeassistant/switch/${DEV_ID}/sw-heater/config -m \
+  '{
    "command_topic": "'${TOPIC_ROOT}'/sw-heater",
    "device": {
     "identifiers": [
@@ -380,8 +478,12 @@ function setup_auto_discovery() {
    "name": "Steering Wheel Heater",
    "device_class": "switch",
    "qos": 1,
-   "unique_id": "'${DEV_ID}'_sw_heater"
-  } {
+   "unique_id": "'${DEV_ID}'_sw_heater",
+   "sw_version": "'${SW_VERSION}'"
+   }'
+
+ eval $MOSQUITTO_PUB_BASE -t homeassistant/switch/${DEV_ID}/sentry-mode/config -m \
+  '{
    "command_topic": "'${TOPIC_ROOT}'/sentry-mode",
    "device": {
     "identifiers": [
@@ -394,8 +496,12 @@ function setup_auto_discovery() {
    "name": "Sentry Mode",
    "device_class": "switch",
    "qos": 1,
-   "unique_id": "'${DEV_ID}'_sentry-mode"
-  } {
+   "unique_id": "'${DEV_ID}'_sentry-mode",
+   "sw_version": "'${SW_VERSION}'"
+   }'
+
+ eval $MOSQUITTO_PUB_BASE -t homeassistant/select/${DEV_ID}/heated_seat_left/config -m \
+  '{
    "command_topic": "'${TOPIC_ROOT}'/heated_seat_left",
    "device": {
     "identifiers": [
@@ -409,8 +515,12 @@ function setup_auto_discovery() {
    "options": ["off", "low", "medium", "high"],
    "qos": 1,
    "icon": "mdi:car-seat-heater",
-   "unique_id": "'${DEV_ID}'_heated_seat_left"
-  } {
+   "unique_id": "'${DEV_ID}'_heated_seat_left",
+   "sw_version": "'${SW_VERSION}'"
+   }'
+
+ eval $MOSQUITTO_PUB_BASE -t homeassistant/select/${DEV_ID}/heated_seat_right/config -m \
+  '{
    "command_topic": "'${TOPIC_ROOT}'/heated_seat_right",
    "device": {
     "identifiers": [
@@ -424,7 +534,8 @@ function setup_auto_discovery() {
    "options": ["off", "low", "medium", "high"],
    "qos": 1,
    "icon": "mdi:car-seat-heater",
-   "unique_id": "'${DEV_ID}'_heated_seat_right"
-  }' | eval $MOSQUITTO_PUB_BASE -t homeassistant/binary_sensor/${DEV_ID}/presence/config -l
+   "unique_id": "'${DEV_ID}'_heated_seat_right",
+   "sw_version": "'${SW_VERSION}'"
+   }'
 
-}
+ }
