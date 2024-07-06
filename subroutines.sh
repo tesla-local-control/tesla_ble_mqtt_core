639718ac74c7d1fd345aee8e8d817d96a163396e
Pierre Belanger <baylanger@users.noreply.github.com>
2024-07-06 01:05:44 -0400

    Fix nPick lines calculation

--- subroutines.sh
+++ subroutines.sh
@@ -144,7 +144,9 @@ function bluetoothctl_read() {
     log_notice "Reading BLE presence data from file $BLECTL_FILE_INPUT"
     nPickMin=0  # min number of lines to pick
     nPickMax=50 # max number of lines to pick
-    nPick=$((RANDOM % (total_lines - nPickMax + nPickMin) + nPickMin))
+    # nPick to be within the file line count and the nPickMax
+    nPick=$((RANDOM % ((finputTotalLines < nPickMax ? \
+      finputTotalLines : nPickMax) - nPickMin + 1) + nPickMin))
     finputTotalLines=$(wc -l < "$BLECTL_FILE_INPUT")
     startLine=$((RANDOM % (finputTotalLines - nPick + 1) + 1)) # Random starting line
 
