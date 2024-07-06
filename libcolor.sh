4e779633f9e605add3e4397f6d165b43ba8062b8
Pierre Belanger <baylanger@users.noreply.github.com>
2024-07-05 23:46:20 -0400

    Move some SC to .shellcheckrc

--- libcolor.sh
+++ libcolor.sh
@@ -18,35 +18,16 @@ else
   RED='\033[0;31m'
 fi
 
-# shellcheck disable=SC3036,SC1089,SC2113
+# shellcheck disable=SC1089
 function log_debug   { [ "$DEBUG" = "true" ] && echo -e "${NOCOLOR}$1" || true; }
 
-# shellcheck disable=SC3036,SC1089,SC2113
 function log_info    { echo -e "${GREEN}$1${NOCOLOR}"; }
-
-# shellcheck disable=SC3036,SC1089,SC2113
 function log_notice  { echo -e "${CYAN}$1${NOCOLOR}"; }
-
-# shellcheck disable=SC3036,SC1089,SC2113
 function log_warning { echo -e "${YELLOW}$1${NOCOLOR}"; }
-
-# shellcheck disable=SC3036,SC1089,SC2113
 function log_error   { echo -e "${MAGENTA}$1${NOCOLOR}"; }
-
-# shellcheck disable=SC3036,SC1089,SC2113
 function log_fatal   { echo -e "${RED}$1${NOCOLOR}"; }
-
-# shellcheck disable=SC3036,SC1089,SC2113
 function log_cyan    { echo -e "${CYAN}$1${NOCOLOR}"; }
-
-# shellcheck disable=SC3036,SC1089,SC2113
 function log_green   { echo -e "${GREEN}$1${NOCOLOR}"; }
-
-# shellcheck disable=SC3036,SC1089,SC2113
 function log_magenta { echo -e "${MAGENTA}$1${NOCOLOR}"; }
-
-# shellcheck disable=SC3036,SC1089,SC2113
 function log_red     { echo -e "${RED}$1${NOCOLOR}"; }
-
-# shellcheck disable=SC3036,SC1089,SC2113
 function log_yellow  { echo -e "${YELLOW}$1${NOCOLOR}"; }
