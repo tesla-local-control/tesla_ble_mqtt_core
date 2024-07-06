590206c8ce61fa6f6024de2042b3372c268c0c7d
Pierre Belanger <baylanger@users.noreply.github.com>
2024-07-05 23:28:54 -0400

    SC disable SC3036

--- libcolor.sh
+++ libcolor.sh
@@ -18,14 +18,25 @@ else
   RED='\033[0;31m'
 fi
 
-log_debug   { [ "$DEBUG" = "true" ] && echo -e "${NOCOLOR}$1" || true; }
+# shellcheck disable=SC3036
+log_debug   { [ "$DEBUG" = "true" ] && echo -e "${NOCOLOR}$1" || true }
+# shellcheck disable=SC3036
 log_info    { echo -e "${GREEN}$1${NOCOLOR}"; }
+# shellcheck disable=SC3036
 log_notice  { echo -e "${CYAN}$1${NOCOLOR}"; }
+# shellcheck disable=SC3036
 log_warning { echo -e "${YELLOW}$1${NOCOLOR}"; }
+# shellcheck disable=SC3036
 log_error   { echo -e "${MAGENTA}$1${NOCOLOR}"; }
+# shellcheck disable=SC3036
 log_fatal   { echo -e "${RED}$1${NOCOLOR}"; }
+# shellcheck disable=SC3036
 log_cyan    { echo -e "${CYAN}$1${NOCOLOR}"; }
+# shellcheck disable=SC3036
 log_green   { echo -e "${GREEN}$1${NOCOLOR}"; }
+# shellcheck disable=SC3036
 log_magenta { echo -e "${MAGENTA}$1${NOCOLOR}"; }
+# shellcheck disable=SC3036
 log_red     { echo -e "${RED}$1${NOCOLOR}"; }
+# shellcheck disable=SC3036
 log_yellow  { echo -e "${YELLOW}$1${NOCOLOR}"; }
