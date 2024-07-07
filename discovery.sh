8d329583cbcacd0f41a0afb2513b71b4bc2c9800
Pierre Belanger <baylanger@users.noreply.github.com>
2024-07-07 18:18:05 -0400

    fix sw_version location

--- discovery.sh
+++ discovery.sh
@@ -25,12 +25,12 @@ function setup_auto_discovery() {
     ],
     "manufacturer": "tesla-local-control",
     "model": "Tesla_BLE",
-    "name": "'${DEV_NAME}'"
+    "name": "'${DEV_NAME}'",
+    "sw_version": "'${SW_VERSION}'"
    },
    "device_class": "presence",
    "name": "Presence",
-   "unique_id": "'${DEV_ID}'_presence",
-   "sw_version": "'${SW_VERSION}'"
+   "unique_id": "'${DEV_ID}'_presence"
   }' | sed ':a;N;$!ba;s/\n//g' | eval $MOSQUITTO_PUB_BASE -t homeassistant/binary_sensor/${DEV_ID}/presence/config -l
 
   echo '{
@@ -41,15 +41,15 @@ function setup_auto_discovery() {
     ],
     "manufacturer": "tesla-local-control",
     "model": "Tesla_BLE",
-    "name": "'${DEV_NAME}'"
+    "name": "'${DEV_NAME}'",
+    "sw_version": "'${SW_VERSION}'"
    },
    "device_class": "update",
    "name": "Generate Keys",
    "payload_press": "generate-keys",
    "qos": 1,
    "unique_id": "'${DEV_ID}'_generate-keys",
-   "entity_category": "config",
-   "sw_version": "'${SW_VERSION}'"
+   "entity_category": "config"
   }' | sed ':a;N;$!ba;s/\n//g' | eval $MOSQUITTO_PUB_BASE -t homeassistant/button/${DEV_ID}/generate-keys/config -l
 
   echo '{
@@ -60,15 +60,15 @@ function setup_auto_discovery() {
     ],
     "manufacturer": "tesla-local-control",
     "model": "Tesla_BLE",
-    "name": "'${DEV_NAME}'"
+    "name": "'${DEV_NAME}'",
+    "sw_version": "'${SW_VERSION}'"
    },
    "device_class": "update",
    "name": "Deploy Key",
    "payload_press": "deploy-key",
    "qos": 1,
    "unique_id": "'${DEV_ID}'_deploy-key",
-   "entity_category": "config",
-   "sw_version": "'${SW_VERSION}'"
+   "entity_category": "config"
   }' | sed ':a;N;$!ba;s/\n//g' | eval $MOSQUITTO_PUB_BASE -t homeassistant/button/${DEV_ID}/deploy-key/config -l
 
   echo '{
@@ -79,15 +79,15 @@ function setup_auto_discovery() {
     ],
     "manufacturer": "tesla-local-control",
     "model": "Tesla_BLE",
-    "name": "'${DEV_NAME}'"
+    "name": "'${DEV_NAME}'",
+    "sw_version": "'${SW_VERSION}'"
    },
    "device_class": "update",
    "name": "Scan Bluetooth",
    "payload_press": "scan-bleln-macaddr",
    "qos": 1,
    "unique_id": "'${DEV_ID}'_scan-bleln-macaddr",
-   "entity_category": "diagnostic",
-   "sw_version": "'${SW_VERSION}'"
+   "entity_category": "diagnostic"
   }' | sed ':a;N;$!ba;s/\n//g' | eval $MOSQUITTO_PUB_BASE -t homeassistant/button/${DEV_ID}/scan-bleln-macaddr/config -l
 
   echo '{
@@ -98,13 +98,13 @@ function setup_auto_discovery() {
     ],
     "manufacturer": "tesla-local-control",
     "model": "Tesla_BLE",
-    "name": "'${DEV_NAME}'"
+    "name": "'${DEV_NAME}'",
+    "sw_version": "'${SW_VERSION}'"
    },
    "name": "Wake Car",
    "payload_press": "wake-up",
    "qos": 1,
-   "unique_id": "'${DEV_ID}'_wake-up",
-   "sw_version": "'${SW_VERSION}'"
+   "unique_id": "'${DEV_ID}'_wake-up"
   }' | sed ':a;N;$!ba;s/\n//g' | eval $MOSQUITTO_PUB_BASE -t homeassistant/button/${DEV_ID}/wake-up/config -l
 
   echo '{
@@ -115,13 +115,13 @@ function setup_auto_discovery() {
     ],
     "manufacturer": "tesla-local-control",
     "model": "Tesla_BLE",
-    "name": "'${DEV_NAME}'"
+    "name": "'${DEV_NAME}'",
+    "sw_version": "'${SW_VERSION}'"
    },
    "name": "Flash Lights",
    "payload_press": "flash-lights",
    "qos": 1,
-   "unique_id": "'${DEV_ID}'_flash-lights",
-   "sw_version": "'${SW_VERSION}'"
+   "unique_id": "'${DEV_ID}'_flash-lights"
   }' | sed ':a;N;$!ba;s/\n//g' | eval $MOSQUITTO_PUB_BASE -t homeassistant/button/${DEV_ID}/flash-lights/config -l
 
   echo '{
@@ -132,13 +132,13 @@ function setup_auto_discovery() {
     ],
     "manufacturer": "tesla-local-control",
     "model": "Tesla_BLE",
-    "name": "'${DEV_NAME}'"
+    "name": "'${DEV_NAME}'",
+    "sw_version": "'${SW_VERSION}'"
    },
    "name": "Honk",
    "payload_press": "honk",
    "qos": 1,
-   "unique_id": "'${DEV_ID}'_honk",
-   "sw_version": "'${SW_VERSION}'"
+   "unique_id": "'${DEV_ID}'_honk"
   }' | sed ':a;N;$!ba;s/\n//g' | eval $MOSQUITTO_PUB_BASE -t homeassistant/button/${DEV_ID}/honk/config -l
 
   echo '{
@@ -149,13 +149,13 @@ function setup_auto_discovery() {
     ],
     "manufacturer": "tesla-local-control",
     "model": "Tesla_BLE",
-    "name": "'${DEV_NAME}'"
+    "name": "'${DEV_NAME}'",
+    "sw_version": "'${SW_VERSION}'"
    },
    "name": "Lock Car",
    "payload_press": "lock",
    "qos": 1,
-   "unique_id": "'${DEV_ID}'_lock",
-   "sw_version": "'${SW_VERSION}'"
+   "unique_id": "'${DEV_ID}'_lock"
   }' | sed ':a;N;$!ba;s/\n//g' | eval $MOSQUITTO_PUB_BASE -t homeassistant/button/${DEV_ID}/lock/config -l
 
   echo '{
@@ -166,13 +166,13 @@ function setup_auto_discovery() {
     ],
     "manufacturer": "tesla-local-control",
     "model": "Tesla_BLE",
-    "name": "'${DEV_NAME}'"
+    "name": "'${DEV_NAME}'",
+    "sw_version": "'${SW_VERSION}'"
    },
    "name": "Unlock Car",
    "payload_press": "unlock",
    "qos": 1,
-   "unique_id": "'${DEV_ID}'_unlock",
-   "sw_version": "'${SW_VERSION}'"
+   "unique_id": "'${DEV_ID}'_unlock"
    }' | sed ':a;N;$!ba;s/\n//g' | eval $MOSQUITTO_PUB_BASE -t homeassistant/button/${DEV_ID}/unlock/config -l
 
   echo '{
@@ -183,13 +183,13 @@ function setup_auto_discovery() {
     ],
     "manufacturer": "tesla-local-control",
     "model": "Tesla_BLE",
-    "name": "'${DEV_NAME}'"
+    "name": "'${DEV_NAME}'",
+    "sw_version": "'${SW_VERSION}'"
    },
    "name": "Auto Seat & Climate",
    "payload_press": "auto-seat-and-climate",
    "qos": 1,
-   "unique_id": "'${DEV_ID}'-auto-seat-and-climate",
-   "sw_version": "'${SW_VERSION}'"
+   "unique_id": "'${DEV_ID}'-auto-seat-and-climate"
    }' | sed ':a;N;$!ba;s/\n//g' | eval $MOSQUITTO_PUB_BASE -t homeassistant/button/${DEV_ID}/auto-seat-and-climate/config -l
 
   echo '{
@@ -200,13 +200,13 @@ function setup_auto_discovery() {
     ],
     "manufacturer": "tesla-local-control",
     "model": "Tesla_BLE",
-    "name": "'${DEV_NAME}'"
+    "name": "'${DEV_NAME}'",
+    "sw_version": "'${SW_VERSION}'"
    },
    "name": "Climate Off",
    "payload_press": "climate-off",
    "qos": 1,
-   "unique_id": "'${DEV_ID}'_climate-off",
-   "sw_version": "'${SW_VERSION}'"
+   "unique_id": "'${DEV_ID}'_climate-off"
    }' | sed ':a;N;$!ba;s/\n//g' | eval $MOSQUITTO_PUB_BASE -t homeassistant/button/${DEV_ID}/climate-off/config -l
 
   echo '{
@@ -217,13 +217,13 @@ function setup_auto_discovery() {
     ],
     "manufacturer": "tesla-local-control",
     "model": "Tesla_BLE",
-    "name": "'${DEV_NAME}'"
+    "name": "'${DEV_NAME}'",
+    "sw_version": "'${SW_VERSION}'"
    },
    "name": "Climate On",
    "payload_press": "climate-on",
    "qos": 1,
-   "unique_id": "'${DEV_ID}'_climate-on",
-   "sw_version": "'${SW_VERSION}'"
+   "unique_id": "'${DEV_ID}'_climate-on"
    }' | sed ':a;N;$!ba;s/\n//g' | eval $MOSQUITTO_PUB_BASE -t homeassistant/button/${DEV_ID}/climate-on/config -l
 
   echo '{
@@ -234,13 +234,13 @@ function setup_auto_discovery() {
     ],
     "manufacturer": "tesla-local-control",
     "model": "Tesla_BLE",
-    "name": "'${DEV_NAME}'"
+    "name": "'${DEV_NAME}'",
+    "sw_version": "'${SW_VERSION}'"
    },
    "name": "Open Trunk",
    "payload_press": "trunk-open",
    "qos": 1,
-   "unique_id": "'${DEV_ID}'_trunk-open",
-   "sw_version": "'${SW_VERSION}'"
+   "unique_id": "'${DEV_ID}'_trunk-open"
    }' | sed ':a;N;$!ba;s/\n//g' | eval $MOSQUITTO_PUB_BASE -t homeassistant/button/${DEV_ID}/trunk-open/config -l
 
   echo '{
@@ -251,13 +251,13 @@ function setup_auto_discovery() {
     ],
     "manufacturer": "tesla-local-control",
     "model": "Tesla_BLE",
-    "name": "'${DEV_NAME}'"
+    "name": "'${DEV_NAME}'",
+    "sw_version": "'${SW_VERSION}'"
    },
    "name": "Close Trunk",
    "payload_press": "trunk-close",
    "qos": 1,
-   "unique_id": "'${DEV_ID}'_trunk-close",
-   "sw_version": "'${SW_VERSION}'"
+   "unique_id": "'${DEV_ID}'_trunk-close"
    }' | sed ':a;N;$!ba;s/\n//g' | eval $MOSQUITTO_PUB_BASE -t homeassistant/button/${DEV_ID}/trunk-close/config -l
 
   echo '{
@@ -268,13 +268,13 @@ function setup_auto_discovery() {
     ],
     "manufacturer": "tesla-local-control",
     "model": "Tesla_BLE",
-    "name": "'${DEV_NAME}'"
+    "name": "'${DEV_NAME}'",
+    "sw_version": "'${SW_VERSION}'"
    },
    "name": "Open Frunk",
    "payload_press": "frunk-open",
    "qos": 1,
-   "unique_id": "'${DEV_ID}'_frunk-open",
-   "sw_version": "'${SW_VERSION}'"
+   "unique_id": "'${DEV_ID}'_frunk-open"
    }' | sed ':a;N;$!ba;s/\n//g' | eval $MOSQUITTO_PUB_BASE -t homeassistant/button/${DEV_ID}/frunk-open/config -l
 
   echo '{
@@ -285,13 +285,13 @@ function setup_auto_discovery() {
     ],
     "manufacturer": "tesla-local-control",
     "model": "Tesla_BLE",
-    "name": "'${DEV_NAME}'"
+    "name": "'${DEV_NAME}'",
+    "sw_version": "'${SW_VERSION}'"
    },
    "name": "Start Charging",
    "payload_press": "charging-start",
    "qos": 1,
-   "unique_id": "'${DEV_ID}'_charging-start",
-   "sw_version": "'${SW_VERSION}'"
+   "unique_id": "'${DEV_ID}'_charging-start"
    }' | sed ':a;N;$!ba;s/\n//g' | eval $MOSQUITTO_PUB_BASE -t homeassistant/button/${DEV_ID}/charging-start/config -l
 
   echo '{
@@ -302,13 +302,13 @@ function setup_auto_discovery() {
     ],
     "manufacturer": "tesla-local-control",
     "model": "Tesla_BLE",
-    "name": "'${DEV_NAME}'"
+    "name": "'${DEV_NAME}'",
+    "sw_version": "'${SW_VERSION}'"
    },
    "name": "Stop Charging",
    "payload_press": "charging-stop",
    "qos": 1,
-   "unique_id": "'${DEV_ID}'_charging-stop",
-   "sw_version": "'${SW_VERSION}'"
+   "unique_id": "'${DEV_ID}'_charging-stop"
    }' | sed ':a;N;$!ba;s/\n//g' | eval $MOSQUITTO_PUB_BASE -t homeassistant/button/${DEV_ID}/charging-stop/config -l
 
   echo '{
@@ -319,13 +319,13 @@ function setup_auto_discovery() {
     ],
     "manufacturer": "tesla-local-control",
     "model": "Tesla_BLE",
-    "name": "'${DEV_NAME}'"
+    "name": "'${DEV_NAME}'",
+    "sw_version": "'${SW_VERSION}'"
    },
    "name": "Open Charge Port",
    "payload_press": "charge-port-open",
    "qos": 1,
-   "unique_id": "'${DEV_ID}'_charge-port-open",
-   "sw_version": "'${SW_VERSION}'"
+   "unique_id": "'${DEV_ID}'_charge-port-open"
    }' | sed ':a;N;$!ba;s/\n//g' | eval $MOSQUITTO_PUB_BASE -t homeassistant/button/${DEV_ID}/charge-port-open/config -l
 
   echo '{
@@ -336,13 +336,13 @@ function setup_auto_discovery() {
     ],
     "manufacturer": "tesla-local-control",
     "model": "Tesla_BLE",
-    "name": "'${DEV_NAME}'"
+    "name": "'${DEV_NAME}'",
+    "sw_version": "'${SW_VERSION}'"
    },
    "name": "Close Charge Port",
    "payload_press": "charge-port-close",
    "qos": 1,
-   "unique_id": "'${DEV_ID}'_charge-port-close",
-   "sw_version": "'${SW_VERSION}'"
+   "unique_id": "'${DEV_ID}'_charge-port-close"
    }' | sed ':a;N;$!ba;s/\n//g' | eval $MOSQUITTO_PUB_BASE -t homeassistant/button/${DEV_ID}/charge-port-close/config -l
 
   echo '{
@@ -353,13 +353,13 @@ function setup_auto_discovery() {
     ],
     "manufacturer": "tesla-local-control",
     "model": "Tesla_BLE",
-    "name": "'${DEV_NAME}'"
+    "name": "'${DEV_NAME}'",
+    "sw_version": "'${SW_VERSION}'"
    },
    "name": "Close Windows",
    "payload_press": "windows-close",
    "qos": 1,
-   "unique_id": "'${DEV_ID}'_windows-close",
-   "sw_version": "'${SW_VERSION}'"
+   "unique_id": "'${DEV_ID}'_windows-close"
    }' | sed ':a;N;$!ba;s/\n//g' | eval $MOSQUITTO_PUB_BASE -t homeassistant/button/${DEV_ID}/windows-close/config -l
 
   echo '{
@@ -370,13 +370,13 @@ function setup_auto_discovery() {
     ],
     "manufacturer": "tesla-local-control",
     "model": "Tesla_BLE",
-    "name": "'${DEV_NAME}'"
+    "name": "'${DEV_NAME}'",
+    "sw_version": "'${SW_VERSION}'"
    },
    "name": "Vent Windows",
    "payload_press": "windows-vent",
    "qos": 1,
-   "unique_id": "'${DEV_ID}'_windows-vent",
-   "sw_version": "'${SW_VERSION}'"
+   "unique_id": "'${DEV_ID}'_windows-vent"
    }' | sed ':a;N;$!ba;s/\n//g' | eval $MOSQUITTO_PUB_BASE -t homeassistant/button/${DEV_ID}/windows-vent/config -l
 
   echo '{
@@ -387,7 +387,8 @@ function setup_auto_discovery() {
     ],
     "manufacturer": "tesla-local-control",
     "model": "Tesla_BLE",
-    "name": "'${DEV_NAME}'"
+    "name": "'${DEV_NAME}'",
+    "sw_version": "'${SW_VERSION}'"
    },
    "name": "Charging Current",
    "unique_id": "'${DEV_ID}'_charging-set-amps",
@@ -396,8 +397,7 @@ function setup_auto_discovery() {
    "mode": "slider",
    "unit_of_measurement": "A",
    "qos": 1,
-   "icon": "mdi:current-ac",
-   "sw_version": "'${SW_VERSION}'"
+   "icon": "mdi:current-ac"
    }' | sed ':a;N;$!ba;s/\n//g' | eval $MOSQUITTO_PUB_BASE -t homeassistant/number/${DEV_ID}/charging-set-amps/config -l
 
   echo '{
@@ -408,7 +408,8 @@ function setup_auto_discovery() {
     ],
     "manufacturer": "tesla-local-control",
     "model": "Tesla_BLE",
-    "name": "'${DEV_NAME}'"
+    "name": "'${DEV_NAME}'",
+    "sw_version": "'${SW_VERSION}'"
    },
    "name": "Charging Current",
    "unique_id": "'${DEV_ID}'_charging-set-amps-override",
@@ -418,8 +419,7 @@ function setup_auto_discovery() {
    "unit_of_measurement": "A",
    "qos": 1,
    "icon": "mdi:current-ac",
-   "entity_category": "diagnostic",
-   "sw_version": "'${SW_VERSION}'"
+   "entity_category": "diagnostic"
    }' | sed ':a;N;$!ba;s/\n//g' | eval $MOSQUITTO_PUB_BASE -t homeassistant/number/${DEV_ID}/charging-set-amps-override/config -l
 
   echo '{
@@ -430,7 +430,8 @@ function setup_auto_discovery() {
     ],
     "manufacturer": "tesla-local-control",
     "model": "Tesla_BLE",
-    "name": "'${DEV_NAME}'"
+    "name": "'${DEV_NAME}'",
+    "sw_version": "'${SW_VERSION}'"
    },
    "name": "Charging Limit",
    "unique_id": "'${DEV_ID}'_charging-set-limit",
@@ -439,8 +440,7 @@ function setup_auto_discovery() {
    "mode": "slider",
    "unit_of_measurement": "%",
    "qos": 1,
-   "icon": "mdi:battery-90",
-   "sw_version": "'${SW_VERSION}'"
+   "icon": "mdi:battery-90"
    }' | sed ':a;N;$!ba;s/\n//g' | eval $MOSQUITTO_PUB_BASE -t homeassistant/number/${DEV_ID}/charging-set-limit/config -l
 
   echo '{
@@ -451,7 +451,8 @@ function setup_auto_discovery() {
     ],
     "manufacturer": "tesla-local-control",
     "model": "Tesla_BLE",
-    "name": "'${DEV_NAME}'"
+    "name": "'${DEV_NAME}'",
+    "sw_version": "'${SW_VERSION}'"
    },
    "name": "Climate Temp",
    "unique_id": "'${DEV_ID}'_climate-set-temp",
@@ -460,8 +461,7 @@ function setup_auto_discovery() {
    "mode": "slider",
    "unit_of_measurement": "°C",
    "qos": 1,
-   "icon": "mdi:temperature",
-   "sw_version": "'${SW_VERSION}'"
+   "icon": "mdi:temperature"
    }' | sed ':a;N;$!ba;s/\n//g' | eval $MOSQUITTO_PUB_BASE -t homeassistant/number/${DEV_ID}/climate-temp/config -l
 
   echo '{
@@ -472,13 +472,13 @@ function setup_auto_discovery() {
     ],
     "manufacturer": "tesla-local-control",
     "model": "Tesla_BLE",
-    "name": "'${DEV_NAME}'"
+    "name": "'${DEV_NAME}'",
+    "sw_version": "'${SW_VERSION}'"
    },
    "name": "Steering Wheel Heater",
    "device_class": "switch",
    "qos": 1,
-   "unique_id": "'${DEV_ID}'_sw-heater",
-   "sw_version": "'${SW_VERSION}'"
+   "unique_id": "'${DEV_ID}'_sw-heater"
    }' | sed ':a;N;$!ba;s/\n//g' | eval $MOSQUITTO_PUB_BASE -t homeassistant/switch/${DEV_ID}/sw-heater/config -l
 
   echo '{
@@ -489,13 +489,13 @@ function setup_auto_discovery() {
     ],
     "manufacturer": "tesla-local-control",
     "model": "Tesla_BLE",
-    "name": "'${DEV_NAME}'"
+    "name": "'${DEV_NAME}'",
+    "sw_version": "'${SW_VERSION}'"
    },
    "name": "Sentry Mode",
    "device_class": "switch",
    "qos": 1,
-   "unique_id": "'${DEV_ID}'_sentry-mode",
-   "sw_version": "'${SW_VERSION}'"
+   "unique_id": "'${DEV_ID}'_sentry-mode"
    }' | sed ':a;N;$!ba;s/\n//g' | eval $MOSQUITTO_PUB_BASE -t homeassistant/switch/${DEV_ID}/sentry-mode/config -l
 
   echo '{
@@ -506,14 +506,14 @@ function setup_auto_discovery() {
     ],
     "manufacturer": "tesla-local-control",
     "model": "Tesla_BLE",
-    "name": "'${DEV_NAME}'"
+    "name": "'${DEV_NAME}'",
+    "sw_version": "'${SW_VERSION}'"
    },
    "name": "Heated Seat Front Left",
    "options": ["off", "low", "medium", "high"],
    "qos": 1,
    "icon": "mdi:car-seat-heater",
-   "unique_id": "'${DEV_ID}'_heater-seat-front-left",
-   "sw_version": "'${SW_VERSION}'"
+   "unique_id": "'${DEV_ID}'_heater-seat-front-left"
    }' | sed ':a;N;$!ba;s/\n//g' | eval $MOSQUITTO_PUB_BASE -t homeassistant/select/${DEV_ID}/heater-seat-front-left/config -l
 
   echo '{
@@ -524,14 +524,14 @@ function setup_auto_discovery() {
     ],
     "manufacturer": "tesla-local-control",
     "model": "Tesla_BLE",
-    "name": "'${DEV_NAME}'"
+    "name": "'${DEV_NAME}'",
+    "sw_version": "'${SW_VERSION}'"
    },
    "name": "Heated Seat Front Right",
    "options": ["off", "low", "medium", "high"],
    "qos": 1,
    "icon": "mdi:car-seat-heater",
-   "unique_id": "'${DEV_ID}'_heater-seat-front-right",
-   "sw_version": "'${SW_VERSION}'"
+   "unique_id": "'${DEV_ID}'_heater-seat-front-right"
    }' | sed ':a;N;$!ba;s/\n//g' | eval $MOSQUITTO_PUB_BASE -t homeassistant/select/${DEV_ID}/heater-seat-front-right/config -l
 
 }
