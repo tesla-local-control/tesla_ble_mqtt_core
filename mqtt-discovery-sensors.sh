9f1f73e21828d47e615ef89f03774572e1047f33
Iain Bullock <59146297+iainbullock@users.noreply.github.com>
2024-12-28 08:45:51 +0000

    Add battery range dummy sensor

--- mqtt-discovery-sensors.sh
+++ mqtt-discovery-sensors.sh
@@ -50,13 +50,34 @@ function setupChargeStateSensors {
     "name": "'${DEVICE_NAME}'",
     "sw_version": "'${SW_VERSION}'"
    },
-   "device_class": "battery",
    "platform": "sensor",
    "icon": "mdi:battery-80",
    "name": "Battery Level",
    "qos": "'${QOS_LEVEL}'",
+   "unit_of_measurement", "%",
+   "suggested_display_precision","0",
    "unique_id": "'${DEVICE_ID}'_charge_state"
   }' | sed ':a;N;$!ba;s/\n//g' | retryMQTTpub 36 10 -t homeassistant/sensor/${DEVICE_ID}/charge_state/config -l
+
+  echo '{
+   "state_topic": "'${TOPIC_ROOT}'/sensor/battery_range",
+   "device": {
+    "identifiers": [
+    "'${DEVICE_ID}'"
+    ],
+    "manufacturer": "tesla-local-control",
+    "model": "Tesla_BLE",
+    "name": "'${DEVICE_NAME}'",
+    "sw_version": "'${SW_VERSION}'"
+   },
+   "platform": "sensor",
+   "icon": "mdi:ruler-square-compass",
+   "name": "Battery Range",
+   "qos": "'${QOS_LEVEL}'",
+   "unit_of_measurement", "miles",
+   "suggested_display_precision","0",
+   "unique_id": "'${DEVICE_ID}'_battery_range"
+  }' | sed ':a;N;$!ba;s/\n//g' | retryMQTTpub 36 10 -t homeassistant/sensor/${DEVICE_ID}/battery_range/config -l
 
   log_debug "setupChargeStateSensors() leaving vin:$vin"
 
