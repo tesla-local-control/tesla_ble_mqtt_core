d26e67cee6c4ce77d56174146ede8d612ba94094
Iain Bullock <59146297+iainbullock@users.noreply.github.com>
2025-01-05 09:58:24 +0000

    Remove comma

--- mqtt-discovery-sensors.sh
+++ mqtt-discovery-sensors.sh
@@ -354,7 +354,7 @@ function setupChargeStateSensors {
     "unit_of_measurement": "°C",
     "suggested_display_precision": "1",   
     "unique_id": "'${DEVICE_ID}'_passenger_temp"
-    "enabled_by_default": "false",
+    "enabled_by_default": "false"
   }' | sed ':a;N;$!ba;s/\n//g' | retryMQTTpub 36 10 -t homeassistant/sensor/${DEVICE_ID}/passenger_temp/config -l 
 
   echo '{
