c3c1bd4b912d72c835e73d38771d7c83b803eaaf
Iain Bullock <59146297+iainbullock@users.noreply.github.com>
2025-01-09 14:41:34 +0000

    More MQTT logs but less spam

--- read-state.sh
+++ read-state.sh
@@ -55,11 +55,11 @@ function poll_state_loop() {
           EXIT_VALUE=$?
           if [ $EXIT_VALUE -ne 0 ] || [ "$rqdValue" != "\"VEHICLE_SLEEP_STATUS_AWAKE\"" ]; then
             log_info "Car is present but asleep VIN:$vin"
-            stateMQTTpub $vin 'false' 'binary_sensor/awake' 
+            stateMQTTpub $vin 'asleep' 'binary_sensor/awake' 
 
           else
             log_info "Car is present and awake VIN:$vin"
-            stateMQTTpub $vin 'true' 'binary_sensor/awake'
+            stateMQTTpub $vin 'awake' 'binary_sensor/awake'
 
             # Check if polling turned off for this car
             if [ "$polling" != "on" ]; then
@@ -114,7 +114,12 @@ function stateMQTTpub() {
   [ $EXIT_STATUS -ne 0 ] &&
     log_error "${MQTT_OUT}" &&
     return 1
-  log_debug "MQTT topic $MQTT_TOPIC succesfully updated to $state"
+  if [ $topic == "binary_sensor/presence_bc" ] || [ $topic == "binary_sensor/asleep" ]
+    # Don't spam the logs for these topics
+    log_debug "MQTT topic $MQTT_TOPIC successfully updated to $state"
+  else
+    log_info "MQTT topic $MQTT_TOPIC successfully updated to $state"
+  fi
 
   return 0
 
