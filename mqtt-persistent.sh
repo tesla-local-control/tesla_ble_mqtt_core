d92fedf974d68dd4c2446e6d7913730bcff7007e
Iain Bullock <59146297+iainbullock@users.noreply.github.com>
2026-04-21 09:36:37 +0100

    Remove 2>&1 for debug

--- mqtt-persistent.sh
+++ mqtt-persistent.sh
@@ -24,7 +24,7 @@ start_mqtt_persistent() {
   if [ -n "$MQTT_USERNAME" ]; then
     log_notice "Starting persistent MQTT client with authentication"
     mosquitto_pub -h "$MQTT_SERVER" -p "$MQTT_PORT" -u "$MQTT_USERNAME" -P "$MQTT_PASSWORD" \
-      -i "$MQTT_CLIENT_ID" --nodelay -l -d < "$MQTT_FIFO_IN" > "$MQTT_FIFO_OUT" 2>&1 &
+      -i "$MQTT_CLIENT_ID" --nodelay -l -d < "$MQTT_FIFO_IN" > "$MQTT_FIFO_OUT" &
   else
     log_notice "Starting persistent MQTT client in anonymous mode"
     mosquitto_pub -h "$MQTT_SERVER" -p "$MQTT_PORT" \
